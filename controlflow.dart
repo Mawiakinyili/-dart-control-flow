f4c3d86fa554a6b5e7c767a713fed554fa07be04
mercymawiyagmail.com <mercymawiya@gmal.com>
2024-03-25 13:53:46 +0300

    dart control flow

---
+++ controlflow.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num = 4;
+
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else if (num == 10) {
+    print('Your number is equal to 10');
+  
+  }
+}
